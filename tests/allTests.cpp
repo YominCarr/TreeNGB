b379d9f15048907fac352ff278444638db0ec013
YominCarr <alex.arth@googlemail.com>
2018-05-15 11:41:00 +0200

    Refactoring of cmake; Added gtest and gmock

---
+++ tests/allTests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+int main ( int argc, char **argv )
+{
+    ::testing::InitGoogleTest ( &argc, argv );
+
+    ::testing::GTEST_FLAG ( shuffle ) =  true;
+
+    return RUN_ALL_TESTS();
+}
+
+
+
